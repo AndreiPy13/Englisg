e12760c0356f21fe41b9533e59681c649ccf1f28
AndreiPy13 <sniper379@mail.ru>
2021-01-30 16:44:46 +0700

    Добавил вывод местоимения из словаря и проверку на соответствие ключ-значение, нужно добавлять уже отгаданные местоимения в другой список, дальше сравнивать эти списки, и либо скопировать словарь и удалять от туда пары ключ-значение, либо  изменять random(чтобы он выдавал рандомные значения из нужного диапазона и не брал те, которые уже были использованы)

--- ENG.py
+++ ENG.py
@@ -1,3 +1,6 @@
+import random
+
+
 
 Pronoun = {
 	'I': 'Я', 
@@ -7,6 +10,10 @@ Pronoun = {
 	'It': 'Оно'
 }
 
+Pronoun_answers = []
+Pronoun_keys = []
+
+
 Themes = {
 	'Местоимения': 'Pronoun'
 }
@@ -15,24 +22,64 @@ topics = [
 'Местоимения',
 ]
 
+def number_objects_in_dicst():
+	how_obj = len(Pronoun)
+	return random.randint(0, how_obj - 1)
+
 
 def choise_theme_start():
 	start = input('Выберите тему: ').capitalize()
 	if start in topics:
 		name = start
-		print('Выбрана тема:' + ' ' + name)
+		return name
 	else: 
 		print('Такой темы нет, попробовать снова?')
 		choise = input('Да/Нет:? ').capitalize()
 		if choise == 'Да':
 			return choise_theme_start()
 		elif choise == 'Нет':
-			return print('Конец игры!')
+			return print('До встречи :)')
 		else:
-			return print('GoodBye :D')
+			return print('Начните заново!')
+
+def start_game():
+	if choise_theme_start() == 'Местоимения':
+		print('Выбрана тема: Местоимения')
+		for pronouns in Pronoun:
+			Pronoun_keys.append(pronouns)
+		random_propoun = Pronoun_keys[number_objects_in_dicst()]
+		question = print('Как переводится: ' + random_propoun + '?')
+		answer = input('Ответ: ')
+		if answer == Pronoun.get(random_propoun):
+			print('Good job, bro!')
+			Pronoun_answers.append(random_propoun)
+			
+		else:
+			print('Restart?')
 
 
-choise_theme_start()
+
+
+
+
+
+start_game()
+print(Pronoun_answers)
+print(Pronoun_keys)
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
 
 
 
